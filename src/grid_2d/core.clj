db142ab50699a174e08bebb428c0a92d9f489295
Max Hunt <max@huntw3.com>
2017-08-13 13:17:55 +0100

    Code is somewhat useful.

--- src/grid_2d/core.clj
+++ src/grid_2d/core.clj
@@ -1,6 +1,24 @@
 (ns grid-2d.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-grid
+  "Make an x by y grid of copies of val."
+  [x y val]
+  (into [] (repeat x (into [] (repeat y val)))))
+
+(defn make-grid-fn
+  "Make an x by y grid, where the entry at (i, j) is the result of (f i j & args)."
+  [x y f & args]
+  (mapv (fn [i] (mapv (fn [j] (apply f i j args))
+                      (range y)))
+        (range x)))
+
+(defn grid-map
+  "Map a function over a grid, returning a grid."
+  [f g]
+  (mapv #(mapv f %) g))
+
+(defn grid-map-range
+  "Like map but only applies f to the grid range from (x1, y1)(inclusive) to (x2, y2)(exlusive)."
+  [f g x1 y1 x2 y2]
+  (reduce #(update-in %1 %2 f) g (for [x (range x1 x2)
+                                       y (range y1 y2)] [x y])))
