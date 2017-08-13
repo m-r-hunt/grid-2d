deba2a906756fdc2e158a7bf8011f20821426d25
Max Hunt <max@huntw3.com>
2017-08-13 13:19:13 +0100

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject grid-2d "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple 2d grid library that operates on nested vectors."
+  :url "https://github.com/m-r-hunt/grid-2d"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
