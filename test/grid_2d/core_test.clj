500f2c638fb00a8aa946b126bf55635870a883c1
Max Hunt <max@huntw3.com>
2017-08-14 21:19:28 +0100

    Start writing tests.

--- test/grid_2d/core_test.clj
+++ test/grid_2d/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [grid-2d.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def test-grid [[0 1 2] [1 2 3] [2 3 4]])
+
+(deftest make-grid-tests
+  (testing "make-grid"
+    (is (= (make-grid 3 3 \a)
+           [[\a \a \a] [\a \a \a] [\a \a \a]])))
+  (testing "make-grid-fn"
+    (is (= (make-grid-fn 3 3 +)
+           test-grid))
+    (is (= (make-grid-fn 3 3 #(vector %1 %2))
+           [[[0 0] [0 1] [0 2]]
+            [[1 0] [1 1] [1 2]]
+            [[2 0] [2 1] [2 2]]]))))
+
+(deftest grid-map-tests
+  (testing "grid-map"
+    (is (= (grid-map inc test-grid)
+           [[1 2 3] [2 3 4] [3 4 5]]))))
