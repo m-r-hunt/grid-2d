ac9c1ba42a1f3978f64c064cb11067ae53d826b8
Max Hunt <max@huntw3.com>
2017-08-14 22:13:46 +0100

    More tests.

--- test/grid_2d/core_test.clj
+++ test/grid_2d/core_test.clj
@@ -14,9 +14,27 @@
     (is (= (make-grid-fn 3 3 #(vector %1 %2))
            [[[0 0] [0 1] [0 2]]
             [[1 0] [1 1] [1 2]]
-            [[2 0] [2 1] [2 2]]]))))
+            [[2 0] [2 1] [2 2]]]))
+    (is (= (make-grid-fn 3 3 (fn [_ _ x] x) \a)
+           [[\a \a \a] [\a \a \a] [\a \a \a]]))))
 
 (deftest grid-map-tests
   (testing "grid-map"
     (is (= (grid-map inc test-grid)
            [[1 2 3] [2 3 4] [3 4 5]]))))
+
+(deftest grid-map-range-tests
+  (testing "grid-map-range"
+    (is (= (grid-map-range inc test-grid 1 1 3 3)
+           [[0 1 2] [1 3 4] [2 4 5]]))
+    (is (= (grid-map-range inc test-grid 1 1 2 2)
+           [[0 1 2] [1 3 3] [2 3 4]]))
+    (is (= (grid-map-range inc test-grid 1 1 1 1)
+           test-grid))))
+
+(deftest grid-reduce-tests
+  (testing "grid-reduce"
+    (is (= (grid-reduce + 0 test-grid)
+           18))
+    (is (= (grid-reduce + 5 test-grid)
+           23))))
