691002ebfadaef2e9662f5bd4bb209b919f2b199
Max Hunt <max@huntw3.com>
2017-08-14 22:14:18 +0100

    Update version number in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grid-2d "0.2.1-SNAPSHOT"
+(defproject grid-2d "0.3.0-SNAPSHOT"
   :description "Simple 2d grid library that operates on nested vectors."
   :url "https://github.com/m-r-hunt/grid-2d"
   :license {:name "Eclipse Public License"
